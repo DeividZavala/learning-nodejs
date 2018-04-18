7f554b530f85c60098e0178c0f0163e099474816
DeividZavala <crusedmoss@gmail.com>
2018-04-17 19:09:51 -0500

    editando stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -19,3 +19,19 @@ exports.getStores = async (req,res) => {
     const stores = await Store.find();
     res.render('stores', {title: 'Stores', stores})
 };
+
+exports.editStore = async (req, res) => {
+    // 1.- Obtener el store dado el ID
+  const store = await Store.findOne({_id : req.params.id});
+  // 2.- Checar si eres dueño del store TODO
+  res.render('editStore', {title: `Editando ${store.name}`, store});
+};
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body,{
+    new: true,
+    runValidators: true
+  }).exec();
+  req.flash('success', `<strong>${store.name}</strong> editado con éxito, <a href="/stores/${store.slug}">Ver Store →</a>`);
+  res.redirect(`/store/${store._id}/edit`);
+};
